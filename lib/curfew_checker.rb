24b9690a5dd830e37bda5d1d2195a21e048eda6a
lksing <leiaxsing@gmail.com>
2018-07-11 16:44:38 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,9 +1,23 @@
+puts "What time is it now? (input the hour based off the 24 hour clock)"
+
+time = gets.to_i
+
 def simple_curfew_checker(time)
-  # code goes here
+    if time > 8
+    puts "Hello!"
+  else
+    puts "Bye!"
+  end
 end
 
+#simple_curfew_checker(9)
+
 def curfew_checker(time)
-  # code goes here
+  if time > 11
+    puts "Your curfew is up! Apparate back to your dorm!"
+  else
+    puts "You have not passed your curfew yet!"
+  end
 end
 
 def complex_curfew_checker(time)
